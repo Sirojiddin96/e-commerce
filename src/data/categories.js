46c36f89eea40a5e78aaa2a908f1816a34f73059
Sirojiddin <Sirojiddin@medit.com>
2022-12-22 23:56:23 +0900

    product categories and products added

---
+++ src/data/categories.js
@@ -0,0 +1,18 @@
+export const productTypes = [
+  {
+    id: "01",
+    name: "Adidas"
+  },
+  {
+    id: "02",
+    name: "Nike"
+  },
+  {
+    id: "03",
+    name: "Puma"
+  },
+  {
+    id: "05",
+    name: "Zara"
+  }
+]
---
+++ src/data/products.js
@@ -0,0 +1,17 @@
+export const products=[
+  {
+    id:"001",
+    name:"Good Shoes",
+    categoryId:"01"
+  },
+  {
+    id:"002",
+    name:"Good Shoes 2",
+    categoryId:"02"
+  },
+  {
+    id:"003",
+    name:"Good Shoes 3",
+    categoryId:"03"
+  }
+]
